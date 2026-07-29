a9740dc8ad944827f712a7242257086ff6faae3b
Andy Liu <andyliu.edu@gmail.com>
2026-07-28 22:06:56 -0700

    Implement RMSNorm layer with forward and residual methods

--- src/vllm-demo/layers/activation.py
+++ src/vllm-demo/layers/activation.py
@@ -3,7 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import time
 
-# SwiGLU MLP:
+# The classic FFN is FFN(x)=W2 @ ​ReLU(W1 @ ​x)
+# This one uses a SwiGLU MLP, and the FFN is FFN(x)=[SiLU(x @ Wg​) ⊙ (x @ Wv​)] @ Wo​
+
+# SwiGLU MLP (Multilayer Perception):
 # - gate_proj learns which features should pass and by how much. SiLU is applied to it.
 # - up_proj learns the feature values/content that will pass through the gate. It normally has no activation before multiplication.
 # - down_proj maps the expanded result back to the model’s hidden size
@@ -12,12 +15,42 @@ import time
 # up   = up_proj(hidden_states)
 # output = down_proj(F.silu(gate_proj(x)) * up_proj(x))
 
+# Advantages: 
+# - More expressive than ReLU or GELU. SwiGLU produces two separate projections and lets one control the other. More flexibility in deciding which information matters for each input
+# - Smooth gradients
+# - Better performance at similar compute
+# Disadvantages:
+# - SwiGLU can require more parameters and matrix multiplication if the intermediate dimension remains unchanged. Usally smaller intermediate dimension
+
 
 class SiluAndMul(nn.Module):
     def __init__(self):
         super().__init__()
 
+    @torch.compile
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # chunk(chunks, dim): splits a tensor into a specific number of smaller parts along a chosen axis
         x, y = x.chunk(2, -1)
         return F.silu(x) * y
+
+if __name__ == "__main__":
+    layer = SiluAndMul().cuda()
+    input_tensor = torch.randn(4000, 8000).cuda()
+
+    for _ in range(10):
+        _ = layer(input_tensor)
+
+    times = []
+    for _ in range(100):
+        torch.cuda.synchronize()
+        cur = time.time()
+        output_tensor = layer(input_tensor)
+        torch.cuda.synchronize()
+        times.append(cur - time.time())
+
+    avg_time = sum(times) / len(times)
+    print(f"Average compliation time = {avg_time * 1000:.3f} ms")
+
+
+
+
---
+++ src/vllm-demo/layers/layernorm.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+import time
+
+# RMSNorm: Root Mean Square Normalization
+# RMS(x) = sqrt(sum(x^2)/len(x) + eps)
+# RMSNorm(x) = x / RMS(x) ⊙ gamma
+# RMSNorm keeps the activation magnitude within a more predictable range, which helps gradients flow through deep Transformer layers.
+# The idea is to Keep the direction and relative feature values, but control the overall magnitude.
+# Usually used in pre-norm
+
+#LayerNorm performs both re-centering and re-scaling.
+#RMSNorm only performs re-scaling.
+
+class RMSNorm(nn.Module):
+    def __init__(self, gamma: torch.Tensor, eps: float = 1e-5):
+        super().__init__()
+        # nn.Parameter make gamma a learnable parameter
+        self.weight = nn.Parameter(gamma.detach().clone())
+        self.eps = eps
+
+    @property
+    def gamma(self):
+        return self.weight
+
+    def rms_forward(self, x: torch.Tensor) -> torch.Tensor:
+        variance = x.pow(2).mean(dim=-1, keepdim=True) + self.eps
+        rms = variance.sqrt()
+        return x / rms * self.weight
+
+    def residual_rms_forward(self, x: torch.Tensor, residual: torch.Tensor) -> torch.Tensor: 
+        # residual help avoid diminishing gradient, perserve information
+        x = x + residual
+        return self.rms_forward(x)
+
+    def forward(self, x: torch.Tensor, residual: torch.Tensor | None = None):
+        if residual:
+            return self.residual_rms_forward(x, residual)
+        else:
+            return self.rms_forward(x)
+
+if __name__ == "__main__":
+    x = torch.randn((4000, 8000)).cuda()
+    gamma = torch.full((8000,), 0.5, dtype=x.dtype, device="cuda")
+
+    layer = RMSNorm(gamma)
+    residual = torch.full_like(x, fill_value=1)
+
+    for _ in range(10):
+        _ = layer(x)
+
+    # without residual
+    times = []
+    for _ in range(100):
+        torch.cuda.synchronize()
+        start = time.time()
+        _ = layer(x)
+        torch.cuda.synchronize()
+        times.append(start - time.time())
+
+    avg_time = sum(times) / len(times)
+    print(f"Without Residual: {avg_time * 1000:.3f} ms")
+
+    # with residual
+    times = []
+    for _ in range(100):
+        torch.cuda.synchronize()
+        start = time.time()
+        _ = layer(x, residual)
+        torch.cuda.synchronize()
+        times.append(start - time.time())
+
+    avg_time = sum(times) / len(times)
+    print(f"With Residual: {avg_time * 1000:.3f} ms")
+
+
+
+
+
