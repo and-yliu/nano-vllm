6158891b8d41aa0b8084a7c6bf438e6e91e498ec
Andy Liu <andyliu.edu@gmail.com>
2026-07-31 23:12:37 -0700

    Add QKVMergedColumnParallelLinear and RowParalleleLinear classes with weight loading functionality

--- src/vllm-demo/layers/linear.py
+++ src/vllm-demo/layers/linear.py
@@ -82,4 +82,87 @@ class MergeColumnParallelLinear(ColumnParallelLinear):
         loaded_weight = loaded_weight.narrow(self.tp_dim, loaded_weight_start_index, shard_size)
         # puts it into the correct parameter section
         param_data.copy_(loaded_weight)
+
+class QKVMergedColumnParallelLinear(ColumnParallelLinear):
+    def __init__(
+        self,
+        input_size: int,
+        head_size: int,
+        num_heads: int,
+        num_kv_heads: int | None = None,
+        bias: bool = False
+    ):
+        self.tp_size = dist.get_world_size()
+        num_kv_heads = num_kv_heads or num_heads
+        self.head_size = head_size
+        # Number of heads for one gpu
+        self.num_heads = num_heads // self.tp_size
+        self.num_kv_heads = num_kv_heads // self.tp_size
+        # the output size of one gpu
+        self.output_size = self.head_size * (self.num_heads + 2 * self.num_kv_heads)
+        total_output_size = head_size * (num_heads + 2 * num_kv_heads)
+
+        super().__init__(input_size, total_output_size, bias)
+
+    def weight_loader(self, param: nn.Parameter, loaded_weight: torch.Tensor, weight_load_id: str):
+        param_data = param.data
+
+        assert weight_load_id in ['q', 'k', 'v']
+
+        # offset: the starting position in locally stored param
+        # shard_size: the length of the portion of param to copy
+        if weight_load_id == "q":
+            offset = 0
+            shard_size = self.head_size * self.num_heads 
+        elif weight_load_id == "k":
+            offset = self.head_size * (self.num_heads)
+            shard_size = self.head_size * self.num_kv_heads
+        elif  weight_load_id == "v":
+            offset = self.head_size * (self.num_heads + self.num_kv_heads)
+            shard_size = self.head_size * self.num_kv_heads
+        else:
+            raise ValueError(f"Unknown weight_load_id: {weight_load_id}")
+
+        param_data = param_data.narrow(self.tp_dim, offset, shard_size)
+
+        # loaded_weight_start_index: the starting position in loaded weight(q weight/ k weight/ v weight)
+        loaded_weight_start_index = shard_size * self.tp_rank
+
+        # shard the original weights and load the portion to the portion in param
+        loaded_weight = loaded_weight.narrow(self.tp_dim, loaded_weight_start_index, shard_size)
+        param_data.copy_(loaded_weight)
+
+class RowParalleleLinear(BaseLinear):
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        bias: bool = False
+    ):
+        self.tp_size = dist.get_world_size()
+        assert output_size % self.tp_size == 0
+        super().__init__(input_size // self.tp_size, output_size, bias, tp_dim=1)
+
+    def weight_loader(self, param: nn.Parameter, loaded_weight: torch.Tensor):
+        param_data = param.data
+
+        # weight size per GPU
+        shard_size = loaded_weight.size(self.tp_dim) 
+        # start index of the loaded weights
+        start_index = self.tp_rank * shard_size
+
+        assert shard_size == param_data.size(self.tp_dim)
+
+        loaded_weight = loaded_weight(self.tp_dim, start_index, shard_size)
+        param_data.copy_(loaded_weight)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        result = F.linear(x, self.weight, self.bias if self.tp_rank == 0 else None)
+        if self.tp_size > 1:
+            dist.all_reduce(result, op=dist.ReduceOp.SUM)
+        return result
+
+    
+        
+
         
